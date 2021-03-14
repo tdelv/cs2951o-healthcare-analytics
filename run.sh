ee606ca75eafab6fcb9cd9b71cd63c9312914bf0
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2021-03-14 01:57:52 -0500

    Update run scripts

--- run.sh
+++ run.sh
@@ -4,13 +4,14 @@
 ############# CSCI 2951-O ##############
 ########################################
 E_BADARGS=65
-if [ $# -ne 1 ]
+if [ $# -le 0 ]
 then
 	echo "Usage: `basename $0` <input>"
 	exit $E_BADARGS
 fi
 	
 input=$1
+args="${@:2}"
 
 # export the ilog license to run the solver
 export ILOG_LICENSE_FILE=/gpfs/main/sys/shared/psfu/local/projects/cplex/ilm/current/linux/access.site.ilm
@@ -19,4 +20,4 @@ export ILOG_LICENSE_FILE=/gpfs/main/sys/shared/psfu/local/projects/cplex/ilm/cur
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/gpfs/main/sys/shared/psfu/local/projects/cplex/CPLEX_Studio_Academic/12.3/x86_64/cplex/bin/x86-64_sles10_4.1
 
 # add the solver jar to the classpath and run
-java -cp /gpfs/main/sys/shared/psfu/local/projects/cplex/CPLEX_Studio_Academic/12.3/x86_64/cplex/lib/cplex.jar:src solver.ip.Main $input
+java -cp /gpfs/main/sys/shared/psfu/local/projects/cplex/CPLEX_Studio_Academic/12.3/x86_64/cplex/lib/cplex.jar:src solver.ip.Main $input $args
